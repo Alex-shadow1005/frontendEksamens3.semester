2e3dd46a287030fbc5a95a4a42afad9a3480474e
MadsHaderup <mads@haderup.net>
2022-05-09 14:59:16 +0200

    updated branch

---
+++ hentgalleri.js
@@ -0,0 +1,47 @@
+const url1 = 'http://localhost:8181/galleri'
+const url = 'http://localhost:8181/get/image/';
+
+
+const getImageNameList = async (url) => {
+    return await fetch(url).then(res => res.json())
+}
+
+const getImage = async (url) => {
+    return await fetch(url).then(res => res.url)
+}
+
+async function createImageMap() {
+    console.log("1")
+    const imageList = await getImageNameList(url1);
+    console.log("2")
+    let currentcol = 1;
+
+
+
+
+    console.log("4")
+
+    imageList.forEach((name) => {
+        console.log(name);
+
+
+        getImage(url + name)
+            .then(result => {
+                console.log(name);
+                console.log(currentcol);
+                let elem = document.createElement("img");
+                elem.setAttribute('src', result);
+                document.getElementById(currentcol.toString()).appendChild(elem);
+                currentcol = currentcol +1;
+                if(currentcol>4){
+                    currentcol = 1;
+                }
+            });
+
+    })
+}
+document.addEventListener('DOMContentLoaded', () => {
+
+    createImageMap();
+
+});
